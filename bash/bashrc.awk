c3d5828692295e82ecb0669fb67185ae1b0f8e07
Jimin Park <pjm950@gmail.com>
2023-11-25 21:21:28 +0900

    Fixed AWK.

--- bash/bashrc.awk
+++ bash/bashrc.awk
@@ -1,19 +1,20 @@
 #!/usr/bin/awk -f
 
-NR == FNR {
-    new = new $0 ORS;
-    next;
+BEGIN {
+    in_block = 0;
 }
 
 $0 ~ end_marker {
     in_block = 0;
 }
 
-!in_block {
+in_block == 0 {
     print;
 }
 
 $0 ~ start_marker {
-    printf "%s", new;
     in_block = 1;
+    while ((getline line < new_config) > 0) {
+        print line;
+    }
 }
--- bash/main.sh
+++ bash/main.sh
@@ -9,8 +9,8 @@ bashrc_start_marker="# START linux-bootstrap"
 bashrc_end_marker="# END linux-bootstrap"
 
 if grep -q "$bashrc_start_marker" ~/.bashrc && grep -q "$bashrc_end_marker" ~/.bashrc; then
-    # temp_bashrc=$(mktemp)
-    awk -v start_marker="$start_marker" -v end_marker="$end_marker" -f "$bashrc_awk_file" "$bashrc_src_file" ~/.bashrc > "$temp_bashrc" # && mv "$temp_bashrc" ~/.bashrc
+    temp_bashrc=$(mktemp)
+    awk -v start_marker="$bashrc_start_marker" -v end_marker="$bashrc_end_marker" -v new_config="$bashrc_src_file" -f "$bashrc_awk_file" ~/.bashrc > "$temp_bashrc" && mv "$temp_bashrc" ~/.bashrc
 else
     echo -e "\n$bashrc_start_marker" >> ~/.bashrc
     cat "$bashrc_src_file" >> ~/.bashrc
