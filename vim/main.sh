76d001c38349ccbdbc7ceadf7299edcc1f35a726
Jimin Park <pjm950@gmail.com>
2023-11-25 21:29:04 +0900

    Fixed bash errors.

--- vim/main.sh
+++ vim/main.sh
@@ -3,6 +3,7 @@
 echo "- Vundle (~/.vim/bundle/Vundle.vim)"
 
 if [ -d "~/.vim/bundle/Vundle.vim" ]; then
+    : # Vundle already installed
 else
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
