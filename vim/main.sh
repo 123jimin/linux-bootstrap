7212f25c36875676f8512c3d1806f5a6aaa2e739
Jimin Park <pjm950@gmail.com>
2023-12-03 23:52:23 +0900

    Fixed incorrect Vundle installation detection.

--- vim/main.sh
+++ vim/main.sh
@@ -2,7 +2,7 @@
 
 echo "- Vundle (~/.vim/bundle/Vundle.vim)"
 
-if [ -d "~/.vim/bundle/Vundle.vim" ]; then
+if [ -d "$HOME/.vim/bundle/Vundle.vim" ]; then
     : # Vundle already installed
 else
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
